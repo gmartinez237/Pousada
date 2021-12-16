c86100d47e40fe8a02af097351d4f78c8ed81541
Guilherme <guilherme.martinez067@gmail.com>
2021-12-16 17:39:05 -0400

    projetinho da facul

---
+++ contato.php
@@ -0,0 +1,11 @@
+<?php
+
+$vnome=$_POST["f_nome"];
+$vtelefone=$_POST["f_tel"];
+$vemail=$_POST["f_email"];
+$vmsg=$_POST["f_msg"];
+
+echo "Nome: ".$vnome."<br/>Telefone: ".$vtelefone."<br/>Email: ".$vemail."<br/>Mensagem: ".$vmsg."<br/>";
+echo "Obrigado!";
+
+?>
